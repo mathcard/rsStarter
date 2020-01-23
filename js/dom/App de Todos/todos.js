3acbe6dbca419246ffd44e464933e838096f79e1
mathcard <matheus.carmo@hotmail.com>
2020-01-23 14:30:36 -0300

    Exemplo Todo

---
+++ js/dom/App de Todos/todos.js
@@ -0,0 +1,60 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+/*var todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar comunidade da rocketseat'
+]; */
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos(){
+    listElement.innerHTML = '';
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+        
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+pos+')');
+
+        var textLink = document.createTextNode('  Excluir');        
+        linkElement.appendChild(textLink);
+        
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addItem(){
+    var text = inputElement.value;
+    
+    todos.push(text);
+    inputElement.value = '';    
+    
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addItem;
+
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+
+function saveToStorage(){
+
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
