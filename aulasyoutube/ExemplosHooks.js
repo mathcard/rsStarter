e7c026e09c12e0b7e494447658f9e704c39d8fbe
mathcard <matheus.carmo@hotmail.com>
2020-02-27 23:22:04 -0300

    Finalizando Aula 01

---
+++ aulasyoutube/ExemplosHooks.js
@@ -0,0 +1,39 @@
+//https://pt-br.reactjs.org/docs/hooks-state.html
+
+//EXEMPLO HOOKS
+import React, { useState } from 'react';
+
+function Example() {
+  // Declarar uma nova variável de state, na qual chamaremos de "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+// EXEMPLO DEVTOOLS
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+}
--- aulasyoutube/src/01iniciando/components/Post.js
+++ aulasyoutube/src/01iniciando/components/Post.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
 import Comment from './Comment';
 
-// Parei com 6:00 minutos
+
 export default class Post extends Component{
     constructor(props){
         super(props);
 
         this.state = {
-            comments: [
-                {text: 'Bom post!'}
-            ]
+            comments: [{text: 'juca'}],
+            newCommentText: ''
         };
+
+        // 
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+
+    handleSubmit(e){
+        // this.setState (Para utilizar precisa informar o react
+        // que ao utilizar o handleSubmit estou referenciando a classe post LINHA - 16 )
+        this.setState({
+            comments: [
+                ...this.state.comments,
+                {text: this.state.newCommentText}
+            ]
+        });
+        
+        e.preventDefault();
+        this.setState({newCommentText: ''});
+    }
+
+    handleTextChange(e){
+        this.setState({newCommentText: e.target.value})
     }
 
     render(){
         return(
             <div>
                 <h2>{this.props.title}</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        value={this.state.newCommentText}
+                        onChange={this.handleTextChange}
+                    />
+                    <button type="submit">Comentar</button>
+                </form>
                 {   this.state.comments.map((comment, index) => {
-                    return <Comment text={comment.text}/>
+                    return <Comment key={index} text={comment.text}/>
                 })
 
                 }
+
             </div>
         );
     }
